068def944d572291e5c66ce48b7e387e937b6ac3
Vladimir Rodchenko <vladimir@rodchenko.by>
2021-03-23 23:50:24 +0300

    initial code for Azure Service Bus sender

---
+++ test/GodelTech.Messaging.AzureServiceBus.Tests/Fakes/FakeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GodelTech.Messaging.AzureServiceBus.Tests.Fakes
+{
+    public class FakeModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
