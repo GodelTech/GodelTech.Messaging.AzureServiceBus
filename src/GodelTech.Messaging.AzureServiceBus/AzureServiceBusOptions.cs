4cd9f3e5dacd292969f494cd84e99126b864afcb
Vladimir Rodchenko <vladimir@rodchenko.by>
2022-07-06 16:37:08 +0300

    dotnet format

--- src/GodelTech.Messaging.AzureServiceBus/AzureServiceBusOptions.cs
+++ src/GodelTech.Messaging.AzureServiceBus/AzureServiceBusOptions.cs
@@ -11,7 +11,7 @@ namespace GodelTech.Messaging.AzureServiceBus
         /// Queue dictionary that stores key value pair of internal queue key and Azure queue name.
         /// </summary>
 #pragma warning disable CA2227 // Collection properties should be read only
-                               // You can suppress the warning if the property is part of a Data Transfer Object (DTO) class.
+        // You can suppress the warning if the property is part of a Data Transfer Object (DTO) class.
         public IDictionary<string, string> Queues { get; set; }
 #pragma warning restore CA2227 // Collection properties should be read only
     }
